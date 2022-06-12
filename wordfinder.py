ce37a04fd07f86e9487535971037e5e8c9d5e8ce
kgagne15 <kristinagagne@ymail.com>
2022-06-12 12:53:11 -0400

    to dos added for wordfinder.py

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,15 @@
 """Word Finder: finds random words from a dictionary."""
 
+#1. instantiate with a path to a file that contains words
+#2. reads that file and makes an attribute that is a list of
+#those words (probably a separate method that is called in __init__)
+#3. prints out a str that says "[number of words read] words read"
+#4. method random is made to return random word from list of words
+#5. the random method should not re-read the entire list but rather
+#work from the list of words that have already been read
+#6. this will need to strip the /n from each word (like the python function
+#further study 5 problem)
 
 class WordFinder:
     ...
+    
