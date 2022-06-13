d5806fe9e5528fa9fa1db1bfb444da72719eadb0
kgagne15 <kristinagagne@ymail.com>
2022-06-13 06:46:48 -0400

    wordfinder class complete

--- wordfinder.py
+++ wordfinder.py
@@ -10,6 +10,36 @@
 #6. this will need to strip the /n from each word (like the python function
 #further study 5 problem)
 
+import random
+
 class WordFinder:
     ...
+    def __init__(self, path):
+        """Initialize the WordFinder class. Prints number of words to find
+
+        Attributes
+        ---------
+        path: name of file to be read
+        words: list of words from that file
+        num_words: number of words in list words
+        """
+        self.path = path
+        self.words = self.read()
+        self.num_words = len(self.words)
+        print(f'{self.num_words} words read')
+
+    def read(self):
+        """Opens file based on path name provided and creates a list 
+        with each word from that file"""
+        word_lst = []
+        file = open(f'{self.path}.txt', 'r')
+        for line in file:
+            line = line.rstrip()
+            word = (line)
+            word_lst.append(word)
+        return word_lst
     
+    def random(self):
+        """selects random index and returns the word from words at that index"""
+        rand_word_idx = random.randint(1, self.num_words)
+        return self.words[rand_word_idx]
